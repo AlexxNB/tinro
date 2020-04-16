84a703941e2699c91c1cd71358d806f1b3fadd9b
AlexxNB <iam@alexxnb.ru>
2020-04-16 16:41:01 +0300

    remove test bundles from git

--- tests/App.svelte
+++ tests/App.svelte
@@ -3,30 +3,38 @@
 	import Child from './Child.svelte';
 </script> 
 
-<p>
-	<a href="/test1">Test1</a>
-	<a href="/test2">Test2</a>
-	<a href="/test3">Test3</a>
-	<a href="/test4/john">Test4</a>
-	<a href="/hello/world">Test hello</a>
-	<a href="//ya.ru">Ya</a>
-	<a href="#huj">Hash</a>
-</p>
+<div>
+	<div>
+		<a href="/test1">Test1</a>
+		<a href="/test2">Test2</a>
+		<a href="/test3">Test3</a>
+		<a href="/test4/john">Test4</a>
+		<a href="/hello/world">Test hello</a>
+		<a href="//ya.ru">Ya</a>
+		<a href="#huj">Hash</a>
+	</div>
 
-<p>
-	<Route>
-		<Route path="/">Main page</Route>
-		<Route path="/test1">TEST1</Route>
-		<Route path="/test2/*">
-			<p>
-				<a href="/test2/sub">Test2 Subpage</a>
-				<a href="/test2/sub3">Test2 NoSubpage</a>
-			</p>
-			<Route path="/sub">TEST2 SUBPAGE</Route>
-			<Route fallback>NOT FOUND SUB</Route>
+	<div>
+		<Route>
+			<Route path="/">Main page</Route>
+			<Route path="/test1">TEST1</Route>
+			<Route path="/test2/*">
+				<p>
+					<a href="/test2/sub">Test2 Subpage</a>
+					<a href="/test2/sub3">Test2 NoSubpage</a>
+				</p>
+				<Route path="/sub">TEST2 SUBPAGE</Route>
+				<Route fallback>NOT FOUND SUB</Route>
+			</Route>
+			<Route path="/hello/:name" let:params>Hello, {params.name}</Route>
+			<Route path="/test4/:name" let:params><Child {params}/></Route>
+			<Route fallback>NOT FOUND</Route>
 		</Route>
-		<Route path="/hello/:name" let:params>Hello, {params.name}</Route>
-		<Route path="/test4/:name" let:params><Child {params}/></Route>
-		<Route fallback>NOT FOUND</Route>
-	</Route>
-</p>
+	</div>
+</div>
+
+
+
+<style>
+	div{display: flex;}
+</style>
